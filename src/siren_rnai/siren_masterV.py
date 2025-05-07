afbebb92bd4dc98493b416a9fa445b334ce54233
Pablo Vargas <pvargasm@enes.unam.mx>
2025-05-07 15:37:32 -0600

    siren_masterV.py

--- src/siren_rnai/siren_masterV.py
+++ src/siren_rnai/siren_masterV.py
@@ -3,7 +3,7 @@ import os
 import subprocess
 import argparse
 
-def run_sirenXII(targets, gene, threads, sensitivity, outdir):
+def run_sirenXII(targets, gene, threads, sensitivity, outdir, sirna_size, min_align_length):
     sirenXII_path = os.path.join(os.path.dirname(__file__), 'sirenXII.py')
     sirenXII_cmd = [
         "python", sirenXII_path,
@@ -11,7 +11,9 @@ def run_sirenXII(targets, gene, threads, sensitivity, outdir):
         "--gene", gene,
         "--threads", str(threads),
         "--sensitivity", sensitivity,
-        "--outdir", outdir
+        "--outdir", outdir,
+        "--sirna_size", str(sirna_size),
+        "--min_align_length", str(min_align_length)
     ]
     subprocess.run(sirenXII_cmd, check=True)
     return os.path.join(outdir, "other_files", "target.fa"), os.path.join(outdir, "off_targets_summary.tsv")
@@ -27,7 +29,7 @@ def run_siren_plotIV(fasta, tsv, outdir):
     ]
     subprocess.run(siren_plot_cmd, check=True)
 
-def run_siren_designVII(fasta, tsv, rnai_length, outdir):
+def run_siren_designVII(fasta, tsv, rnai_length, outdir, threads):
     rnai_tsv_output = os.path.join(outdir, "rna_sequences_with_scores.tsv")
     graph_output = os.path.join(outdir, "rna_sequences_plot.png")
     siren_designVII_path = os.path.join(os.path.dirname(__file__), 'siren_designVIII.py')
@@ -36,6 +38,7 @@ def run_siren_designVII(fasta, tsv, rnai_length, outdir):
         "--target_path", fasta,
         "--off_targets_summary_path", tsv,
         "--rnai_seq_length", str(rnai_length),
+        "--threads", str(threads),
         "--out", rnai_tsv_output
     ]
     subprocess.run(siren_design_cmd, check=True)
@@ -43,7 +46,7 @@ def run_siren_designVII(fasta, tsv, rnai_length, outdir):
 def main():
     parser = argparse.ArgumentParser(
         prog="SIREN",
-        description="""
+        description=r"""
 
  ____ ___ ____  _____ _   _ 
 / ___|_ _|  _ \| ____| \ | |
@@ -68,22 +71,27 @@ Example for Arabidopsis:
     )
     parser.add_argument("--targets", required=True, help="FASTA file containing organism cDNA sequences.")
     parser.add_argument("--gene", required=True, help="Gene name or partial FASTA gene header to identify the target gene.")
-    parser.add_argument("--threads", type=int, default=6, help="Number of threads for parallel processing (default: 6).")
+    parser.add_argument("--threads", type=int, default=8, help="Number of threads for parallel processing (default: 8).")
     parser.add_argument("--sensitivity", choices=["high", "medium", "low"], default="low",
                         help="Sensitivity level for siRNA generation (default: low).")
     parser.add_argument("--rnai_length", type=int, default=200, help="Base RNAi sequence length (default: 200).")
     parser.add_argument("--outdir", default="siren_results", help="Directory to store output files (default: siren_results).")
+    parser.add_argument("--sirna_size", type=int, default=21, help="Length of siRNAs (default: 21).")
+
     parser.add_argument("--min_align_length", type=int, help="Minimum alignment length for off-target detection (default: sirna_size - 4).")
     args = parser.parse_args()
+    if args.min_align_length is None:
+        args.min_align_length = args.sirna_size - 4
+
 
     # Create output directory if it doesn't exist.
     if not os.path.exists(args.outdir):
         os.makedirs(args.outdir)
 
     # Run the three steps in sequence.
-    target_fa, off_targets_summary_tsv = run_sirenXII(args.targets, args.gene, args.threads, args.sensitivity, args.outdir)
+    target_fa, off_targets_summary_tsv = run_sirenXII(args.targets, args.gene, args.threads, args.sensitivity, args.outdir, args.sirna_size, args.min_align_length)
     run_siren_plotIV(target_fa, off_targets_summary_tsv, args.outdir)
-    run_siren_designVII(target_fa, off_targets_summary_tsv, args.rnai_length, args.outdir)
+    run_siren_designVII(target_fa, off_targets_summary_tsv, args.rnai_length, args.outdir, args.threads)
 
 if __name__ == "__main__":
     main()
